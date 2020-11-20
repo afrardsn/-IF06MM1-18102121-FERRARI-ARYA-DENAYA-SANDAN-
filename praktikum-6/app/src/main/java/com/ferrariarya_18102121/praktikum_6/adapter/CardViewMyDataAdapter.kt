ac46eecd18122f1a7fbfc667ce39a6510220348f
Ferrari Arya Denaya <72550767+afrardsn@users.noreply.github.com>
2020-11-20 22:56:45 +0800

    7.2 Scrolling Activity

--- praktikum-6/app/src/main/java/com/ferrariarya_18102121/praktikum_6/adapter/CardViewMyDataAdapter.kt
+++ praktikum-6/app/src/main/java/com/ferrariarya_18102121/praktikum_6/adapter/CardViewMyDataAdapter.kt
@@ -1,5 +1,7 @@
 package com.ferrariarya_18102121.praktikum_6.adapter
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,10 +12,11 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.ferrariarya_18102121.praktikum_6.DetailActivity
 import com.ferrariarya_18102121.praktikum_6.MyData
 import com.ferrariarya_18102121.praktikum_6.R
 
-class CardViewMyDataAdapter(private val listMyDatas: ArrayList<MyData>) :
+class CardViewMyDataAdapter(private val listMyDatas: ArrayList<MyData>, val context: Context) :
     RecyclerView.Adapter<CardViewMyDataAdapter.CardViewViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewViewHolder {
         val view: View =
@@ -42,10 +45,10 @@ class CardViewMyDataAdapter(private val listMyDatas: ArrayList<MyData>) :
             ).show()
         }
         holder.itemView.setOnClickListener {
-            Toast.makeText(
-                holder.itemView.context, "Kamu memilih " +
-                        listMyDatas[holder.adapterPosition].name, Toast.LENGTH_SHORT
-            ).show()
+            val moveWithObjectIntent = Intent(context, DetailActivity::class.java)
+            moveWithObjectIntent.putExtra(DetailActivity.EXTRA_MYDATA, myData)
+            context.startActivity(moveWithObjectIntent)
+
         }
     }
 
