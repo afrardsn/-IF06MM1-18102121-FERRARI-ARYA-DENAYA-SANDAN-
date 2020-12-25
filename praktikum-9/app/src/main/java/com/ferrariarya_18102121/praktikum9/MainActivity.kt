4a9024dbb7d9047e11f385c63f99983b4e5d4288
Ferrari Arya Denaya <72550767+afrardsn@users.noreply.github.com>
2020-12-25 10:39:18 +0800

    9.3 Tugas, memindahkan button setting ke action bar

--- praktikum-9/app/src/main/java/com/ferrariarya_18102121/praktikum9/MainActivity.kt
+++ praktikum-9/app/src/main/java/com/ferrariarya_18102121/praktikum9/MainActivity.kt
@@ -3,15 +3,19 @@ package com.ferrariarya_18102121.praktikum9
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatDelegate
 import com.ferrariarya_18102121.praktikum9.data.SettingModel
 import com.ferrariarya_18102121.praktikum9.databinding.ActivityMainBinding
 import com.ferrariarya_18102121.praktikum9.preference.SettingPreference
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     companion object {
         private const val REQUEST_CODE = 100
     }
+
     private lateinit var settingModel: SettingModel
     private lateinit var binding: ActivityMainBinding
     private lateinit var mSettingPreference: SettingPreference
@@ -40,15 +44,31 @@ class MainActivity : AppCompatActivity() {
         }
         binding.settingModel = settingModel
     }
-    fun openSetting(){
-        val intent = Intent(this@MainActivity, SettingPreferenceActivity::class.java)
-        startActivityForResult(intent, REQUEST_CODE)
-    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE) {
             if (resultCode == SettingPreferenceActivity.RESULT_CODE) {
                 showExistingPreference()
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.setting -> {
+                val intent = Intent(this@MainActivity, SettingPreferenceActivity::class.java)
+                startActivityForResult(intent, REQUEST_CODE)
             }
         }
     }
