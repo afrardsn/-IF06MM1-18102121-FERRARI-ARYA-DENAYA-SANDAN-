5307a72468436f5b14abae10e77ac9472c21ad8f
Ferrari Arya Denaya <72550767+afrardsn@users.noreply.github.com>
2021-01-02 20:17:33 +0800

    10.2 SQLite step 1-14

---
+++ praktikum-10/app/src/main/java/com/ferrariarya_18102121/praktikum10/data/Quote.kt
@@ -0,0 +1,13 @@
+package com.ferrariarya_18102121.praktikum10.data
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Quote(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var category: String? = null,
+    var date: String? = null
+) : Parcelable
