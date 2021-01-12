da007ce0f9c36130938525a45b673d20a00b222b
Ferrari Arya Denaya <72550767+afrardsn@users.noreply.github.com>
2021-01-12 16:53:06 +0800

    11.2 Konfigurasi Firebase

---
+++ praktikum-11/app/src/main/java/com/ferrariarya_18102121/praktikum11/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.ferrariarya_18102121.praktikum11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
